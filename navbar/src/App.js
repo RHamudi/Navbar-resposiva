57b3ca1519a149a9e6dbf5f118376d8a24042116
RHamudi <ramonramos.silva16@gmail.com>
2022-08-12 10:10:40 -0300

    first commit

--- navbar/src/App.js
+++ navbar/src/App.js
@@ -1,27 +1,8 @@
-import {useState} from 'react'
 
 function App() {
-  const [active, setMode] = useState(false);
-  const toggleMode = ()=> {
-    setMode(!active)
-  }
 
   return (
-    <div className="App">
-      <div className={active ? 'icon iconActive' : 'icon'}  >
-        <div className="hamburguer hamburguerIcon" onClick={toggleMode}></div>
-      </div>
-      <div className={active ? 'menu menuOpen' : 'menu menuClose'}>
-        <div className="list">
-          <ul className="listItems">
-            <li>Home</li>
-            <li>Products</li>
-            <li>Contact</li>
-            <li>About US</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <div>App</div>
   );
 }
 
